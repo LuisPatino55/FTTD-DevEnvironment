dc7e9437fbd0616221223bdcc9cddd9726466cf9
LuisPatino55 <144381605+LuisPatino55@users.noreply.github.com>
2023-10-23 19:55:29 -0700

    added function to the battle history list, added method to move warriors around lists

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,54 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    public enum WarriorList { Player, Opponent, Store, Dead }
+
+    [Serializable]
+    public struct BattleHistory
+    {
+        public string OpponentName;
+        public int OpponentID;
+        public int OpponentLevel;
+        public bool Win;
+    }
+
     public static DataManager Instance;
     [Space(10)]
     public List<Warrior> ConfiguredOpponentWarriors = new();
     [Space(10)]
     public List<Warrior> StoreInventoryWarriors = new();
     [Space(10)]
-    public Dictionary<string, int> battleHistory = new();
+    public List<Warrior> PlayerBarracks = new();
+    [Space(10)]
+    public List<Warrior> DeadWarriors = new();
+    [Space(10)]
+    public Warrior[] ArenaWarriors = new Warrior[2];
+
+    // key will be the warrior ID, value will be a list of battle history
+    public Dictionary<int, List<BattleHistory>> battleHistory = new();
+
 
     private void Awake()
     {
-        if(Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-
+        if(Instance != null) { Destroy(gameObject); return; }               // Singleton pattern
+        Instance = this; DontDestroyOnLoad(gameObject);
     }
 
-
-    // ======================================================================================== Battle History 
-    public void AddToBattleHistory(string key, int value)   // Battle History addition 
+    public void AddWarriorToList(Warrior warrior, WarriorList list)         //add warrior to appropriate list
     {
-        battleHistory[key] = value;
+        switch (list)
+        {
+            case WarriorList.Opponent:
+                ConfiguredOpponentWarriors.Add(warrior);
+                break;
+            case WarriorList.Store:
+                StoreInventoryWarriors.Add(warrior);
+                break;
+            case WarriorList.Player:
+                PlayerBarracks.Add(warrior);
+                break;
+            case WarriorList.Dead:
+                DeadWarriors.Add(warrior);
+                break;
+            default:
+                Debug.Log("Error: WarriorList not found");
+                break;
+        }
+
+    }
+    public void HistoryEnrty(int id, Warrior opponent)
+    {
+        BattleHistory historyEntry = new BattleHistory(); 
+        historyEntry.OpponentName = opponent.WarriorName;
+        historyEntry.OpponentID = opponent.WarriorID;
+        historyEntry.OpponentLevel = opponent.combatLevel;
+        historyEntry.Win = true;
+        battleHistory[id].Add(historyEntry);
     }
 
-    public int GetValueForBattleHistory(string key)
-    {
-        if (battleHistory.TryGetValue(key, out int value))
-        {
-            return value;
-        }
-        else
-        {
-            return -1; // or any other appropriate default value
-        }
-    }
-    public void PrintBattleHistory()                                                // Print the Battle History to the console
-    {
-        foreach (var kvp in battleHistory)
-        {
-            Debug.Log("Key: " + kvp.Key + ", Value: " + kvp.Value);
-        }
-    }
+
+
+
+
+
 
     public void PrintStats(List<Warrior> holdingList)                               // prints full roster on list 
     {
